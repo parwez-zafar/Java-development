4b23a1ca7633faea226b79d9146f1010228a398e
parwez-zafar <parwezzafar3@gmail.com>
2024-10-14 12:35:20 +0530

    mend

--- java-concept/Enum.java
+++ java-concept/Enum.java
@@ -1,17 +1,44 @@
 
-enum Status{
+enum Status {
     Running, Failed, Pending, Success;
 }
 
 public class Enum {
-   
-    public static void main(String a[])
-    {
+
+    public static void main(String a[]) {
         Status s = Status.Running;
         System.out.println(s.ordinal()); // ordinal gives index
 
         Status[] s2 = Status.values(); // get all value as an array
         System.out.println(s2[0]);
-        
+
+        // enum with if else and switch
+
+        s = Status.Pending;
+        if (s == Status.Running)
+            System.out.println("All Good");
+        else if (s == Status.Failed)
+            System.out.println("Try Again");
+        else if (s == Status.Pending)
+            System.out.println("Please Wait");
+        else
+            System.out.println("Done");
+
+        switch (s) {
+            case Running:
+                System.out.println("All Good");
+
+                break;
+            case Failed:
+                System.out.println("Try Again");
+                break;
+            case Pending:
+                System.out.println("Please Wait");
+                break;
+            default:
+                System.out.println("Done");
+                break;
+        }
+
     }
 }
