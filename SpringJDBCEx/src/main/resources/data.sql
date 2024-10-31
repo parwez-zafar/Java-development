6a0807e7eb55fd459d7a8570140c575ac9361815
parwez-zafar <parwezzafar3@gmail.com>
2024-10-31 11:55:38 +0530

    Spring JDBC

---
+++ SpringJDBCEx/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollNo, name, marks) values (101, 'name1', 59);
+insert into student (rollNo, name, marks) values (102, 'name2', 98);
+insert into student (rollNo, name, marks) values (103, 'name3', 67);
